60b442ec52c397afbc0175173d29a77abdec797e
vinod <vinod_parasa@srmap.edu.in>
2025-10-17 11:38:22 +0530

    Updating an Article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,12 +21,22 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
     def article_params
       params.require(:article).permit(:title, :body)
     end
 end
-# Submitted form data is put into the params Hash, alongside captured route parameters. Thus, the create action can access the submitted title via
-# params[:article][:title] and the submitted body via params[:article][:body]
-#  However, we must still specify what values are allowed in that Hash. Otherwise, a malicious user could potentially submit extra form fields
-#  and overwrite private data.
