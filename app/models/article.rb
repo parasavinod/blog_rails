4f52bfb710d05cf11875100b0fd460ba1023967a
vinod <vinod_parasa@srmap.edu.in>
2025-10-21 15:24:00 +0530

    Associating Models

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,8 @@
 class Article < ApplicationRecord
+  has_many :comments
+
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
 end
-
-#  creating a resource is a multi-step process. Handling invalid user input is another step of that process.
-#  Rails provides a feature called validations to help us deal with invalid user input. Validations are rules that are checked before a model object is saved.
-#  If any of the checks fail, the save will be aborted, and appropriate error messages will be added to the errors attribute of the model object.
+# These two declarations enable a good bit of automatic behavior. For example, if you have an instance variable @article containing an article,
+#  you can retrieve all the comments belonging to that article as an array using @article.comments.end
